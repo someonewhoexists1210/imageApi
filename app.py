a84ad55c05121da2df82248911d55c1c2ed1e910
someonewhoexists1210 <130904011+someonewhoexists1210@users.noreply.github.com>
2024-08-04 21:36:28 +0530

    first commit

---
+++ app.py
@@ -0,0 +1,18 @@
+from flask import Flask, render_template, request
+from dotenv import load_dotenv
+import os
+  
+app = Flask(__name__)  
+ 
+@app.route('/') 
+def home():  
+    return render_template('index.html')
+
+@app.route('/search', methods=['POST'])
+def search():
+    search_query = request.form['query']
+    return render_template('search.html', query=search_query)
+
+  
+if __name__ =='__main__':  
+    app.run(debug = True)  
