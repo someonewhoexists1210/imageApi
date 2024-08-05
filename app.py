4d7577ae5c2e6ac6033dd79bf2bbf0465bd32082
someonewhoexists1210 <130904011+someonewhoexists1210@users.noreply.github.com>
2024-08-05 14:32:59 +0530

    got api key and make query

--- app.py
+++ app.py
@@ -1,9 +1,24 @@
-from flask import Flask, render_template, request
+from flask import Flask, jsonify, render_template, request
 from dotenv import load_dotenv
 import os
-  
+import requests
+
+API = os.getenv('API_KEY')
+CX = os.getenv('SEARCHENGINE_ID')  
 app = Flask(__name__)  
  
+def search_images(query):
+    url = f"https://www.googleapis.com/customsearch/v1"
+    params = {
+        'q': query,
+        'cx': CX,
+        'key': API,
+        'searchType': 'image',
+        'num': 10  # Number of results to return
+    }
+    response = requests.get(url, params=params)
+    return response.json()
+
 @app.route('/') 
 def home():  
     return render_template('index.html')
@@ -11,7 +26,8 @@ def home():
 @app.route('/search', methods=['POST'])
 def search():
     search_query = request.form['query']
-    return render_template('search.html', query=search_query)
+    result = search_images(search_query)
+    return jsonify(result)
 
   
 if __name__ =='__main__':  
