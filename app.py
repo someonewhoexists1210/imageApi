a251493dadf597ff8d432ec13ff7a318aba775c5
someonewhoexists1210 <130904011+someonewhoexists1210@users.noreply.github.com>
2024-08-05 17:41:16 +0530

    Got search results + styled results page

--- app.py
+++ app.py
@@ -14,9 +14,10 @@ def search_images(query):
         'cx': CX,
         'key': API,
         'searchType': 'image',
-        'num': 10  # Number of results to return
     }
     response = requests.get(url, params=params)
+    if not response.ok:
+        return {"error": response.json()}
     return response.json()
 
 @app.route('/') 
@@ -26,8 +27,21 @@ def home():
 @app.route('/search', methods=['POST'])
 def search():
     search_query = request.form['query']
-    result = search_images(search_query)
-    return jsonify(result)
+    results = search_images(search_query)
+    if 'error' in results:
+        return jsonify(results), 500
+    if 'items' in results:
+        images = []
+        for item in results['items']:
+            images.append({
+                'title': item.get('title'),
+                'image_link': item.get('link'),  # Direct image URL
+                'thumbnail_link': item['image'].get('thumbnailLink'),  # Thumbnail URL
+                'context_link': item['image'].get('contextLink')  # Page containing the image
+            })
+        return render_template('search.html', images=images, query=search_query)
+    else:
+        return jsonify({"error": "No images found"}), 404
 
   
 if __name__ =='__main__':  
